c19a39f152287e83ae9d37fd6edde1e4d061d96f
Prem Patil <Premvishwa6847@gmail.com>
2025-03-29 09:21:50 +0530

    Update Contents.swift

--- 1 - Getting Started/4 - Control Flow/Lab - Control Flow.playground/Pages/5. App Exercise - Target Heart Rate.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/4 - Control Flow/Lab - Control Flow.playground/Pages/5. App Exercise - Target Heart Rate.xcplaygroundpage/Contents.swift
@@ -7,9 +7,22 @@
  
  Create constants `isInTarget`, `isBelowTarget`, and `isAboveTarget` that equal expressions that evaluate to whether or not `currentHR` is between the lower and upper bounds, below the lower bound, and above the upper bound, respectively. Then write an if-else-if statement that will print "You're right on track!" if the user is inside the target zone, "You're doing great, but try to push it a bit!" if the user is below the target zone, and "You're on fire! Slow it down just a bit." if the user is above the target zone.
  */
-let targetLowerBound = 120
-let targetUpperBound = 150
-let currentHR = 147
+let lowerBound = 120
+let upperBound = 150
+
+let currentHR = 135
+
+let isInTarget = currentHR >= lowerBound && currentHR <= upperBound
+let isBelowTarget = currentHR < lowerBound
+let isAboveTarget = currentHR > upperBound
+
+if isInTarget {
+    print("You're right on track!")
+} else if isBelowTarget {
+    print("You're doing great, but try to push it a bit!")
+} else if isAboveTarget {
+    print("You're on fire! Slow it down just a bit.")
+}
 
 /*:
 [Previous](@previous)  |  page 5 of 9  |  [Next: Exercise - Switch Statements](@next)
