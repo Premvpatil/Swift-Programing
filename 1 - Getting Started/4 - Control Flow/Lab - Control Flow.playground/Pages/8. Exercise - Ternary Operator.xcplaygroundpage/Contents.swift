d6fea1cc8626a288918c2c94311e2abb0db1f895
Prem Patil <Premvishwa6847@gmail.com>
2025-03-29 09:34:05 +0530

    Update Contents.swift

--- 1 - Getting Started/4 - Control Flow/Lab - Control Flow.playground/Pages/8. Exercise - Ternary Operator.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/4 - Control Flow/Lab - Control Flow.playground/Pages/8. Exercise - Ternary Operator.xcplaygroundpage/Contents.swift
@@ -3,15 +3,22 @@
  
  Refactor the code below so that `largest` is declared and assigned to in one line using the ternary operator.
  */
+//let number1 = 14
+//let number2 = 25
+//
+//var largest: Int
+//if number1 > number2 {
+//    largest = number1
+//} else {
+//    largest = number2
+//}
+
 let number1 = 14
 let number2 = 25
 
-var largest: Int
-if number1 > number2 {
-    largest = number1
-} else {
-    largest = number2
-}
+// Using the ternary operator to assign the largest number
+let largest = number1 > number2 ? number1 : number2
+print(largest)
 /*:
 [Previous](@previous)  |  page 8 of 9  |  [Next: App Exercise - Ternary Messages](@next)
  */
